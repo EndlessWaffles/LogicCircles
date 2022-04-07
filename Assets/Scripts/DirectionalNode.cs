d979d0964d5eee4e977d6133621b3e1b19c5c5da
Joe <joeydixon999@gmail.com>
2022-04-07 10:33:40 -0400

    added directional node logic

--- Assets/Scripts/DirectionalNode.cs
+++ Assets/Scripts/DirectionalNode.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class DirectionalNode : MonoBehaviour
 {
+    SpriteRenderer sr;
     public Sprite none;
     public Sprite left;
     public Sprite right;
     public string current;
-    SpriteRenderer sr;
-
     // Start is called before the first frame update
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+        sr.sprite = left;
         SwitchSprite();
     }
 
@@ -25,16 +25,19 @@ public class DirectionalNode : MonoBehaviour
 
     void SwitchSprite()
     {
-        if(current == "left")
+        if (current == "left")
         {
+            Debug.Log("Choose Left");
             sr.sprite = left;
         }
-        else if(current == "right")
+        else if (current == "right")
         {
+            Debug.Log("Choose Right");
             sr.sprite = right;
         }
         else
         {
+            Debug.Log("Choose None");
             sr.sprite = none;
         }
     }
